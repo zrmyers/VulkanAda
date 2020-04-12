92e1d2da73e08020554a85b3a60106021dcc5660
Zane Myers <myers554@umn.edu>
2020-04-12 12:02:09 -0500

    added deploy_pages.sh scripts

---
+++ deploy_pages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DATE=date
+
+# Deploy the generated documentation to github pages.
+mv docs .docs
+git checkout gh-pages
+git rm -rf docs
+mv .docs docs
+git add docs
+git commit -m "Deploy docs on $DATE"
+git push
+git checkout -
