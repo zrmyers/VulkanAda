60afec5cdc724b4bb3b86bac6013ecef2a3c2d6a
Zane Myers <myers554@umn.edu>
2020-04-12 21:10:42 -0500

    documentation generation updates

--- src/vulkan-math/gentype/vulkan-math-gentype.ads
+++ src/vulkan-math/gentype/vulkan-math-gentype.ads
@@ -21,7 +21,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 --------------------------------------------------------------------------------
---< @group Vulkan Math Generics
+--< @group Vulkan Math GenType
 --------------------------------------------------------------------------------
 --< @summary
 --< This generic package provides constructors, getters and setters, swizzlers,
--- src/vulkan-math/gentype/vulkan-math-genutype.ads
+++ src/vulkan-math/gentype/vulkan-math-genutype.ads
@@ -21,23 +21,59 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 --------------------------------------------------------------------------------
--- This package describes a generic Unsigned Integer Vulkan Math type.
---------------------------------------------------------------------------------
 with Vulkan.Math.GenType;
 with Vulkan.Math.GenBType;
 use Vulkan.Math.GenBType;
 
+--------------------------------------------------------------------------------
+--< @group Vulkan Math GenType
+--------------------------------------------------------------------------------
+--< @summary
+--< This package describes any length vector of Vkm_Uint type.
+--<
+--< @description
+--< Provides an instatiation of the generic GenType  package with a Base_Type of 
+--< Vkm_Uint. This is used to provide the GenUType subtype for the Vulkan Math 
+--< library.
+--------------------------------------------------------------------------------
 package Vulkan.Math.GenUType is
     pragma Preelaborate;
     pragma Pure;
 
+    ----------------------------------------------------------------------------
+    --< @private
+    --< An instance of the generic GenType package, with Vkm_Uint as the Base_Type.
     package GUT is new Vulkan.Math.GenType(
         Base_Type => Vkm_Uint, Image => Vkm_Uint'Image);
 
+    --< A subtype of the instantiated Vkm_GenType that represents the GenUType 
+    --< described in the GLSL specification.
     subtype Vkm_GenUType is GUT.Vkm_GenType;
 
     ----------------------------------------------------------------------------
     -- Generic Operations
+    ----------------------------------------------------------------------------
+    ---------------------------------------------------------------------------- 
+    --< @summary
+    --< Apply function for parameters of Vkm_Uint and Vkm_Bool type to vectors.
+    --<
+    --< @description
+    --< Applies a supplied function component wise on two GenUType vectors and
+    --< a GenBType vector returning a GenUType vector.
+    --<
+    --<    RVU := [Func(IVU1.x, IVU2.x, IVB1.x) ... Func(IVU1.w, IVU2.w, IVB1.w)]
+    --<
+    --< @param IVU1
+    --< The first input GenUType parameter.
+    --<
+    --< @param IVU2
+    --< The second input GenUType parameter.
+    --<
+    --< @param IVB1
+    --< The first input GenBType parameter.
+    --<
+    --< @return
+    --< The result GenUType vector, RVU.
     ----------------------------------------------------------------------------
     generic
         with function Func(ISF1, ISF2 : in     Vkm_Uint;
@@ -46,6 +82,26 @@ package Vulkan.Math.GenUType is
     function Apply_Func_IVU_IVU_IVB_RVU(IVU1, IVU2 : in     Vkm_GenUType;
                                         IVB1       : in     Vkm_GenBType) return Vkm_GenUType;
 
+
+    ---------------------------------------------------------------------------- 
+    --< @summary
+    --< Apply function for parameters of Vkm_Uint and Vkm_Bool type to vectors.
+    --<
+    --< @description
+    --< Applies a supplied function component wise on two GenUType vectors and
+    --< returns a GenBType vector.
+    --<
+    --<    RVB := [Func(IVU1.x, IVU2.x) ... Func(IVU1.w, IVU2.w)]
+    --<
+    --< @param IVU1
+    --< The first input GenUType parameter.
+    --<
+    --< @param IVU2
+    --< The second input GenUType parameter.
+    --<
+    --< @return
+    --< The result GenBType vector, RVB.
+    ----------------------------------------------------------------------------
     generic
         with function Func(ISU1, ISU2 : in     Vkm_Uint) return Vkm_Bool;
     function Apply_Func_IVU_IVU_RVB(IVU1, IVU2 : in     Vkm_GenUType) return Vkm_GenBType;
